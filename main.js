0b931b3775a190ff351319defe8c5c9bc7719782
gztsao <69137180+gztsao@users.noreply.github.com>
2023-12-13 16:25:04 +0800

    test qrcode

--- main.js
+++ main.js
@@ -509,17 +509,15 @@ let aryOld = [
 
     // 條件為id="qrcode"的位置更新網站網址並產生 QR code "變數rul"
     // let QRcode = "https://www.google.com"; //無效ans."eferenceError: Can't find variable: QRCode"
+    document.getElementById('qrcode').innerHTML = ""; //先將qrcode清空下一行產生新qr
     let qrcode = new QRCode(document.getElementById("qrcode"), {
-      text: "https://www.google.com",
-      width: 128,
-      height: 128,
+      text: url,//將網址放入選項
+      width: 200,
+      height: 200,
       colorDark : "#000000",
       colorLight : "#ffffff",
       correctLevel : QRCode.CorrectLevel.H
     });
-    qrcode.clear(); // clear the code.
-    qrcode.makeCode(url); // make another code.
-
 
   }
   
